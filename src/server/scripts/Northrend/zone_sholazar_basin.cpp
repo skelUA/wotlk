af6bb20b3cb752f815ceaf25dce950a95313f288
volodymyrtkachenko <v.tkachenko.it@gmail.com>
2023-10-17 12:20:25 +0300

    Fix quest q12726

--- src/server/scripts/Northrend/zone_sholazar_basin.cpp
+++ src/server/scripts/Northrend/zone_sholazar_basin.cpp
@@ -52,7 +52,7 @@ public:
                 cr->SetDisplayId(cr->GetDisplayId() == NPC_SOWAW_WATER_MODEL ? NPC_SOWAW_WIND_MODEL : NPC_SOWAW_WATER_MODEL);
                 if (Player* player = cr->GetCharmerOrOwnerPlayerOrPlayerItself())
                 {
-                    player->KilledMonsterCredit(cr->GetDisplayId() == NPC_SOWAW_WATER_MODEL ? 29008 : 29009);
+//                    player->KilledMonsterCredit(cr->GetDisplayId() == NPC_SOWAW_WATER_MODEL ? 29008 : 29009);
                     CreatureTemplate const* ct = sObjectMgr->GetCreatureTemplate(cr->GetDisplayId() == NPC_SOWAW_WIND_MODEL ? NPC_SOWAW_WIND_ELEMENTAL : NPC_SOWAW_WATER_ELEMENTAL);
                     for (uint8 i = 0; i < MAX_CREATURE_SPELLS; ++i)
                         cr->m_spells[i] = ct->spells[i];
