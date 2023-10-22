57e0a0a98917a79809497d8c51ad2510f5c0c40d
Serge <t4gr1m@gmail.com>
2023-10-22 04:20:04 +0300

    Plans: Storm Helm droprate fix

---
+++ data/sql/custom/tenstorms/05_item_23623_droprate.sql
@@ -0,0 +1,41 @@
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 24093) AND (`Item` IN (23623));
+DELETE FROM `reference_loot_template` WHERE (`Entry` = 10006) AND (`Item` IN (23623));
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 19209) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(19209, 23623, 0, 0.06, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 22845) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(22845, 23623, 0, 0.03, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 21231) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(21231, 23623, 0, 0.03, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 20036) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(20036, 23623, 0, 0.02, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 21519) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(21519, 23623, 0, 0.017, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 18312) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(18312, 23623, 0, 0.017, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 18634) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(18634, 23623, 0, 0.013, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16539) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(16539, 23623, 0, 0.013, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 17839) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(17839, 23623, 0, 0.01, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 15551) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(15551, 23623, 0, 0.01, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 16409) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(16409, 23623, 0, 0.009, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 18220) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(18220, 23623, 0, 0.009, 0, 1, 0, 1, 1, '');
+DELETE FROM `creature_loot_template` WHERE (`Entry` = 17464) AND (`Item` IN (23623));
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`, `Comment`) VALUES
+(17464, 23623, 0, 0.009, 0, 1, 0, 1, 1, '');
