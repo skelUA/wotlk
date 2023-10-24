723dd4b984c4a84820aac7544ab825132887997d
Serge <t4gr1m@gmail.com>
2023-10-24 17:30:43 +0300

    Quel'dalar quests transferring fix

---
+++ data/sql/custom/tenstorms/06_queldalar_quests_migration.sql
@@ -0,0 +1,6 @@
+INSERT INTO player_factionchange_quests (alliance_id,horde_id)
+	VALUES (24454,24558);
+INSERT INTO player_factionchange_quests (alliance_id,horde_id)
+	VALUES (24553,24594);
+INSERT INTO player_factionchange_quests (alliance_id,horde_id)
+	VALUES (24796,24800);
