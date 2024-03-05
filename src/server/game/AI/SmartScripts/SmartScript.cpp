e8f67b0f2e6b340ebabef6781d00fc339e7baf2e
skelUA <v.tkachenko.it@gmail.com>
2024-03-05 18:48:34 +0200

    Test SpellIgnoreLOS (2)

--- src/server/game/AI/SmartScripts/SmartScript.cpp
+++ src/server/game/AI/SmartScripts/SmartScript.cpp
@@ -635,10 +635,10 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
                     bool isTargetRooted = target->ToUnit()->HasUnitState(UNIT_STATE_ROOT);
                     // To prevent running back and forth when OOM, we must have more than 10% mana.
                     bool canCastSpell = me->GetPowerPct(POWER_MANA) > 10.0f && spellInfo->CalcPowerCost(me, spellInfo->GetSchoolMask()) < (int32)me->GetPower(POWER_MANA) && !me->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_SILENCED);
-                    bool SpellIgnoreLOS = spellInfo->HasAttribute(SPELL_ATTR2_IGNORE_LINE_OF_SIGHT);
+                    bool isSpellIgnoreLOS = spellInfo->HasAttribute(SPELL_ATTR2_IGNORE_LINE_OF_SIGHT);
 
                     // If target is rooted we move out of melee range before casting, but not further than spell max range.
-                    if (isWithinLOSInMap && isWithinMeleeRange && isRangedAttack && isTargetRooted && canCastSpell)
+                    if ((isWithinLOSInMap || isSpellIgnoreLOS) && isWithinMeleeRange && isRangedAttack && isTargetRooted && canCastSpell)
                     {
                         failedSpellCast = true; // Mark spellcast as failed so we can retry it later
                         float minDistance = std::max(meleeRange, spellMinRange) - distanceToTarget + NOMINAL_MELEE_RANGE;
@@ -647,13 +647,13 @@ void SmartScript::ProcessAction(SmartScriptHolder& e, Unit* unit, uint32 var0, u
                     }
 
                     // Let us not try to cast spell if we know it is going to fail anyway. Stick to chasing and continue.
-                    if (distanceToTarget > spellMaxRange && isWithinLOSInMap)
+                    if (distanceToTarget > spellMaxRange && (isWithinLOSInMap || isSpellIgnoreLOS))
                     {
                         failedSpellCast = true;
                         CAST_AI(SmartAI, me->AI())->SetCombatMove(true, std::max(spellMaxRange - NOMINAL_MELEE_RANGE, 0.0f));
                         continue;
                     }
-                    else if (distanceToTarget < spellMinRange || !isWithinLOSInMap)
+                    else if (distanceToTarget < spellMinRange || (!isWithinLOSInMap)
                     {
                         failedSpellCast = true;
                         CAST_AI(SmartAI, me->AI())->SetCombatMove(true);
