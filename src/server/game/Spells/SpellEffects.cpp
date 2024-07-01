abaf013255128846fbb2d39ea8a9ba09d6433716
SkelUA <v.tkachenko.it@gmail.com>
2024-07-01 18:11:42 +0300

    Test (4_)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5691,11 +5691,11 @@ LOG_INFO("server.loading", "EffectKillCreditPersonal 1");
 void Spell::EffectKillCredit(SpellEffIndex effIndex)
 {
     LOG_INFO("server.loading", "EffectKillCredit 1");
-//    if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT_TARGET)
-//        return;
+    if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT_TARGET)
+        return;
 LOG_INFO("server.loading", "EffectKillCredit 2");
-//    if (!unitTarget)
-//        return;
+    if (!unitTarget)
+        return;
 LOG_INFO("server.loading", "EffectKillCredit 3");
     Player* player = unitTarget->GetCharmerOrOwnerPlayerOrPlayerItself();
     if (!player)
