4a615720e1fa1fc83e0a5b28744b2f8aec64b6a7
SkelUA <v.tkachenko.it@gmail.com>
2024-09-19 11:46:53 +0300

    Revert "Revert "fix(Core/AI): Add back leash update. (#19916)""
    This reverts commit fc9207bd186dcbb1fb25e9c0efa86519c217ee23.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13534,6 +13534,9 @@ void Unit::SetInCombatWith(Unit* enemy, uint32 duration)
         }
     }
 
+    if (Creature* pCreature = ToCreature())
+        pCreature->UpdateLeashExtensionTime();
+
     SetInCombatState(false, enemy, duration);
 }
 
