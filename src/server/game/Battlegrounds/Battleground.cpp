896ed0027e9a52dfc7644d393f1020d1279643bc
SkelUA <v.tkachenko.it@gmail.com>
2024-10-08 14:11:47 +0300

    Test bg (3)

--- src/server/game/Battlegrounds/Battleground.cpp
+++ src/server/game/Battlegrounds/Battleground.cpp
@@ -1741,7 +1741,7 @@ void Battleground::HandleKillPlayer(Player* victim, Player* killer)
             if (creditedPlayer->GetBgTeamId() == killer->GetBgTeamId() && (creditedPlayer == killer || creditedPlayer->IsAtGroupRewardDistance(victim)))
             {
               UpdatePlayerScore(creditedPlayer, SCORE_HONORABLE_KILLS, 1);
-              creditedPlayer->KilledMonsterCredit(killer->GetBgTeamId()== TEAM_HORDE ? NPC_QUEST_PVP_KILL_ALLIANCE_BG : NPC_QUEST_PVP_KILL_HORDE_BG);
+              creditedPlayer->KilledMonsterCredit(NPC_QUEST_PVP_KILL_BG);
             }
 
         }
