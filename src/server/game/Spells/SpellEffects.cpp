606a1b17cad12539ff54929fa1fe464801d466fa
SkelUA <v.tkachenko.it@gmail.com>
2024-12-21 23:41:30 +0200

    Revert "last test 3"
    This reverts commit 5c2c161204c546a129a163bc70246a4a60bdec3c.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -3360,8 +3360,8 @@ void Spell::EffectWeaponDmg(SpellEffIndex effIndex)
                     {
                         damage = 60;
                         // about +4 base spell dmg per level
-                        if (m_caster && m_caster->getLevel() >= 60)
-                            damage += (m_caster->getLevel() - 60) * 4;
+                        if (unitCaster && unitCaster->getLevel() >= 60)
+                            damage += (unitCaster->getLevel() - 60) * 4;
                         break;
                     }
                 }
