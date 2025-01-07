636dbe5bea11dbf73b215b55890c11a5fa065762
SkelUA <v.tkachenko.it@gmail.com>
2025-01-07 18:51:23 +0200

    Fix ProcessSpellQueue crash

--- src/server/game/Entities/Player/PlayerUpdates.cpp
+++ src/server/game/Entities/Player/PlayerUpdates.cpp
@@ -2338,6 +2338,12 @@ void Player::ProcessSpellQueue()
     {
         PendingSpellCastRequest& request = SpellQueue.front(); // Peek at the first spell
         SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(request.spellId);
+        if (!spellInfo)
+        {
+            LOG_ERROR("entities.player", "Player::ProcessSpellQueue: Invalid spell {}", request.spellId);
+            SpellQueue.clear();
+            break;
+        }
         if (CanExecutePendingSpellCastRequest(spellInfo))
         {
             ExecuteOrCancelSpellCastRequest(&request);
