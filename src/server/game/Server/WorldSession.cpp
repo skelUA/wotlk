a1f7d5dfb243c1b646e96d21473bea3dfaec985e
SkelUA <v.tkachenko.it@gmail.com>
2025-01-25 15:43:19 +0200

    Auction max packet size

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -1554,17 +1554,17 @@ uint32 WorldSession::DosProtection::GetMaxPacketCounterAllowed(uint16 opcode) co
         case CMSG_REPORT_PVP_AFK:                       // not profiled
         case CMSG_AUCTION_LIST_ITEMS:                   // not profiled
             {
-                maxPacketCounterAllowed = 500;
+                maxPacketCounterAllowed = 0;
                 break;
             }
         case CMSG_AUCTION_LIST_BIDDER_ITEMS:            // not profiled
             {
-                maxPacketCounterAllowed = 500;
+                maxPacketCounterAllowed = 0;
                 break;
             }
         case CMSG_AUCTION_LIST_OWNER_ITEMS:             // not profiled
             {
-                maxPacketCounterAllowed = 500;
+                maxPacketCounterAllowed = 0;
                 break;
             }
 
