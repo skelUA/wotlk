70243b85b4c8fb0da5fff64ddd52e08f485d0592
SkelUA <v.tkachenko.it@gmail.com>
2025-02-07 00:17:43 +0200

    Revert "fix(Scripts/SunwellPlateau): Felmyst should be invincible while flying (#21336)"
    This reverts commit 8f450686b96f6d7ab9ab455dce4b16b13494d03d.

--- src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
+++ src/server/scripts/EasternKingdoms/SunwellPlateau/boss_felmyst.cpp
@@ -163,7 +163,6 @@ struct boss_felmyst : public BossAI
             me->HandleEmoteCommand(EMOTE_ONESHOT_LAND);
             me->SetDisableGravity(false);
             me->SendMovementFlagUpdate();
-            SetInvincibility(false);
 
             me->m_Events.AddEventAtOffset([&] {
                 me->SetReactState(REACT_AGGRESSIVE);
@@ -239,7 +238,6 @@ struct boss_felmyst : public BossAI
         me->HandleEmoteCommand(EMOTE_ONESHOT_LIFTOFF);
         me->SetDisableGravity(true);
         me->SendMovementFlagUpdate();
-        SetInvincibility(true);
 
         me->m_Events.AddEventAtOffset([&] {
             me->GetMotionMaster()->MovePoint(POINT_AIR, me->GetPositionX(), me->GetPositionY(), me->GetPositionZ() + 15.0f, false, true);
