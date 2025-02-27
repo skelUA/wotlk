0a45cae87e0110dd4d027713cf077560928957da
vrachv <vrachv@outlook.com>
2025-02-27 17:43:13 +0200

    Some typo fix

--- data/sql/custom/tenstorms/event_ahnqiraj.sql
+++ data/sql/custom/tenstorms/event_ahnqiraj.sql
@@ -109,8 +109,6 @@ DELETE FROM `gameobject_questender` WHERE (`quest` = 8743);
 INSERT INTO `gameobject_questender` (`id`, `quest`) VALUES
 (180717, 8743);
 
-UPDATE `gameobject` SET `id` = '180717' WHERE (`guid` = '
-
-');
+UPDATE `gameobject` SET `id` = '180717' WHERE (`guid` = '49451');
 -- revert
 -- UPDATE `gameobject` SET `id` = '180718' WHERE (`guid` = '49451');
